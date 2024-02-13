4a920c9ad30c6e1389ba16b5ef33c779a611ba8c
vshwanilgv <vishwanibhagya2002@gmail.com>
2024-02-13 12:11:36 +0530

    homePage-added chaanges to the home page layout

--- pages/generatedQr.tsx
+++ pages/generatedQr.tsx
@@ -17,17 +17,17 @@ function homePage(){
                     <div className="flex rounded-lg  ">
                         <Image className='rounded-lg drop-shadow-lg' src="/images/auth.png" width={40} height={30} alt=''/> 
                     </div>
-                    <div className='flex justify-center border-2 rounded-2xl  mt-0.5'>
+                    <div className='flex justify-center mt-0.5'>
                         <p className='text-4xl text-white font-extrabold px-5'>QR Gen</p>
                     </div>
 
 
                     <div className='flex justify-end absolute top-0 right-0'>
                         <div className='px-3'>
-                            <button className='bg-purple2 text-white font-bold text-xl rounded-2xl w-1/8 p-1 mt-1'>Login</button>
+                            <button className='bg-purple2 text-white font-bold text-xl rounded-2xl w-1/5 p-1 mt-1'>Login</button>
                         </div>
                         <div className='px-3'>
-                            <button className='bg-purple2 text-white font-bold text-xl rounded-2xl w-1/8 p-1 mt-1'>SignUp</button>
+                            <button className='bg-purple2 text-white font-bold text-xl rounded-2xl w-1/5 p-1 mt-1'>SignUp</button>
                         </div>
                     </div>
                 </div>
--- pages/homePage.tsx
+++ pages/homePage.tsx
@@ -14,20 +14,20 @@ function homePage(){
             <div className='w-full h-16 bg-purple1 flex-auto rounded-2xl p-2'>
 
                 <div className='flex justify-start'>
-                    <div className="flex rounded-lg  ">
+                    <div className="flex rounded-lg ">
                         <Image className='rounded-lg drop-shadow-lg' src="/images/auth.png" width={40} height={30} alt=''/> 
                     </div>
-                    <div className='flex justify-center border-2 rounded-2xl  mt-0.5'>
+                    <div className='flex justify-center  mt-0.5'>
                         <p className='text-4xl text-white font-extrabold px-5'>QR Gen</p>
                     </div>
 
 
                     <div className='flex justify-end absolute top-0 right-0 p-2'>
-                        <div className='px-3'>
-                            <button className='bg-purple2 text-white font-bold text-xl rounded-2xl w-1/8 p-1 mt-1'>Login</button>
+                        <div className='px-4' >
+                            <button className='bg-purple2 text-white font-bold text-xl rounded-xl w-{250} p-1 mt-1 px-3'> Login </button>
                         </div>
-                        <div className='px-3'>
-                            <button className='bg-purple2 text-white font-bold text-xl rounded-2xl w-1/8 p-1 mt-1'>SignUp</button>
+                        <div className='px-4'>
+                            <button className='bg-purple2 text-white font-bold text-xl rounded-xl w-{250} p-1 mt-1 px-3'>SignUp</button>
                         </div>
                     </div>
                 </div>
@@ -35,7 +35,7 @@ function homePage(){
                 
             </div>
             
-            <div className="flex justify-center my-5 mt-5">
+            <div className="flex justify-center  my-10 mt-10 p-5">
                 <Image className='rounded-lg drop-shadow-lg' src="/images/qr.png" width={400} height={300} alt=''/>
             </div>
 
